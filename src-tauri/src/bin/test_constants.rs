5ea09258755bf1bc2d18521836986dd7ed9515fb
Madbeat14 <savedra.pablo@gmail.com>
2026-03-07 01:07:07 -0300

    Remove Tauri version completely

--- src-tauri/src/bin/test_constants.rs
+++
@@ -1,9 +0,0 @@
-use windows::Win32::Media::Audio::{eCapture, eRender, eConsole, eMultimedia, eCommunications};
-
-fn main() {
-    println!("eCapture: {:?}", eCapture);
-    println!("eRender: {:?}", eRender);
-    println!("eConsole: {:?}", eConsole);
-    println!("eMultimedia: {:?}", eMultimedia);
-    println!("eCommunications: {:?}", eCommunications);
-}
--- src-tauri/src/main.rs
+++
@@ -1,6 +0,0 @@
-// Tauri desktop entry point
-#![windows_subsystem = "windows"]
-
-fn main() {
-    app_lib::run();
-}
